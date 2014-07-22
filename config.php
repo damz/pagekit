ecab7d4d4f70a8c8785a008ef5b4db09b3d3add2
Damien Tournoud <damien@commerceguys.com>
2014-07-22 21:09:18 +0200

    Add platform files.

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+  'database' => array(
+    'default' => 'mysql',
+    'connections' => array(
+      'mysql' => array(
+        'host' => 'database.internal',
+        'user' => '',
+        'password' => '',
+        'dbname' => 'main',
+        'prefix' => 'pk_',
+      ),
+    ),
+  ),
+);
